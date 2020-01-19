d7204c3c5123162e3fe0c1a954aecfedcf378271
Tyler <42395039+tktran11@users.noreply.github.com>
2020-01-18 23:33:00 -0800

    Fixed Heroku importing issues

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'NotASecret' # this is just an example, it shouldnt be shown
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    
 jwt = JWT(app, authenticate, identity) # create /auth endpoint
 
 api.add_resource(Store, '/store/<string:name>')
